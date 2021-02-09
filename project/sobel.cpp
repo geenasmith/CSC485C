fbdb98f7d9f9263fb9307cf1528e404a5dd6c7d8
Geena Smith <geena_smith@hotmail.com>
2021-02-08 16:14:05 -0800

    rudimentary sobel implemented. updated .gitignore

--- project/sobel.cpp
+++ project/sobel.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -7,15 +8,93 @@
 using namespace cv;
 
 /*
-compile: g++ -std=c++11 sobel.cpp -o sobel `pkg-config --cflags --libs opencv`
+compile (cause pkg-config is annoying): g++ -std=c++11 sobel.cpp -o sobel `pkg-config --cflags --libs opencv`
+run: ./sobel <input image>
 */
 
-void preprocessing()
+Mat preprocessing(char *filename)
 {
+    /*
+    Apply preprocessing:
+      - read image as grayscale
+      - convert to float equivalent (needed to access elements in Mat object)
+      - apply gaussian filter to smooth image (removes noise that might be considered an edge)
+      - pad the image with a 1px border of 0s for the sobel filter
+    */
+
+    // read in image as grayscale
+    // Mat is an OpenCV data structure
+    Mat raw_image = imread(filename, IMREAD_GRAYSCALE);
+
+    if (raw_image.empty())
+    {
+        std::cout << "Could not read image: " << filename << std::endl;
+        return raw_image;
+    }
+
+    // convert image to CV_32F (equivalent to a float)
+    Mat image;
+    raw_image.convertTo(image, CV_32F);
+
+    // gaussian filter to remove noise
+    GaussianBlur(image, image, Size(3, 3), 0, 0);
+
+    // pad image with 1 px of 0s
+    Mat padded_image;
+    copyMakeBorder(image, padded_image, 1, 1, 1, 1, BORDER_CONSTANT, 0);
+
+    return padded_image;
 }
 
-void sobel()
+Mat sobel(Mat padded_image)
 {
+    /*
+        Applies the sobel filtering to the padded image and normalizes to [0,255]
+    */
+
+    // Define convolution kernels Gx and Gy
+    int g_x[3][3] = {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}};
+    int g_y[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+
+    // Filtered image definitions
+    int n_rows = padded_image.rows - 1;
+    int n_cols = padded_image.cols - 1;
+    Mat sobel_image(n_rows, n_cols, CV_32F);
+
+    // loop through calculating G_x and G_y
+    // mag is sqrt(G_x^2 + G_y^2)
+    for (int r = 0; r < n_rows; r++)
+    {
+        for (int c = 0; c < n_cols; c++)
+        {
+            float mag_x = padded_image.at<float>(r, c) * g_x[0][0] +
+                          padded_image.at<float>(r, c + 1) * g_x[0][1] +
+                          padded_image.at<float>(r, c + 2) * g_x[0][2] +
+                          padded_image.at<float>(r + 1, c) * g_x[1][0] +
+                          padded_image.at<float>(r + 1, c + 1) * g_x[1][1] +
+                          padded_image.at<float>(r + 1, c + 2) * g_x[1][2] +
+                          padded_image.at<float>(r + 2, c) * g_x[2][0] +
+                          padded_image.at<float>(r + 2, c + 1) * g_x[2][1] +
+                          padded_image.at<float>(r + 2, c + 2) * g_x[2][2];
+
+            float mag_y = padded_image.at<float>(r, c) * g_y[0][0] +
+                          padded_image.at<float>(r, c + 1) * g_y[0][1] +
+                          padded_image.at<float>(r, c + 2) * g_y[0][2] +
+                          padded_image.at<float>(r + 1, c) * g_y[1][0] +
+                          padded_image.at<float>(r + 1, c + 1) * g_y[1][1] +
+                          padded_image.at<float>(r + 1, c + 2) * g_y[1][2] +
+                          padded_image.at<float>(r + 2, c) * g_y[2][0] +
+                          padded_image.at<float>(r + 2, c + 1) * g_y[2][1] +
+                          padded_image.at<float>(r + 2, c + 2) * g_y[2][2];
+
+            sobel_image.at<float>(r, c) = sqrt(pow(mag_x, 2) + pow(mag_y, 2));
+        }
+    }
+
+    // normalize to 0-255
+    normalize(sobel_image, sobel_image, 0, 255, NORM_MINMAX, CV_8UC1);
+
+    return sobel_image;
 }
 
 int main(int argc, char **argv)
@@ -27,34 +106,17 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    // read in image as grayscale
-    Mat image = imread(argv[1], IMREAD_GRAYSCALE);
+    Mat padded_img = preprocessing(argv[1]);
 
-    if (image.empty())
+    if (padded_img.empty())
     {
-        std::cout << "Could not read image: " << argv[1] << std::endl;
         return 1;
     }
 
-    // gaussian filter to remove noise
-    GaussianBlur(image, image, Size(3, 3), 0, 0);
+    Mat sobel_img = sobel(padded_img);
 
-    imshow("Display window", image);
-    int k = waitKey(0);
-
-    // pad image with 1 px of 0s
-
-    // convert to vector of vectors
-
-    // loop through calculating G_x and G_y
-    // mag is sqrt(G_x^2 + G_y^2)
-
-    // int g_x[3][3] = {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}};
-    // int g_y[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
-
-    // normalize to 0-255
-
-    // save edge detection
+    imshow("Detected Edges", sobel_img);
+    waitKey(0);
 
     return 0;
 }
