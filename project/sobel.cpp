950b64ef12aad878f641f8ff4ad11c6c0a421575
Geena Smith <geena_smith@hotmail.com>
2021-02-18 20:06:31 -0800

    fixed segfault in array sobel

--- project/sobel.cpp
+++ project/sobel.cpp
@@ -12,29 +12,13 @@ compile (cause pkg-config is annoying): g++ -std=c++11 sobel.cpp -o sobel `pkg-c
 run: ./sobel <input image>
 */
 
-Mat preprocessing(char *filename)
+Mat preprocessing(Mat image)
 {
     /*
     Apply preprocessing:
-      - read image as grayscale
-      - convert to float equivalent (needed to access elements in Mat object)
       - apply gaussian filter to smooth image (removes noise that might be considered an edge)
       - pad the image with a 1px border of 0s for the sobel filter
     */
-
-    // read in image as grayscale
-    // Mat is an OpenCV data structure
-    Mat raw_image = imread(filename, IMREAD_GRAYSCALE);
-
-    if (raw_image.empty())
-    {
-        std::cout << "Could not read image: " << filename << std::endl;
-        return raw_image;
-    }
-
-    // convert image to CV_32F (equivalent to a float)
-    Mat image;
-    raw_image.convertTo(image, CV_32F);
 
     // gaussian filter to remove noise
     GaussianBlur(image, image, Size(3, 3), 0, 0);
@@ -110,15 +94,15 @@ Mat sobel_array(Mat padded_image)
     // Filtered image definitions
     int n_rows = padded_image.rows - 1;
     int n_cols = padded_image.cols - 1;
-	
+
     float sobel_array[n_rows][n_cols];
 
     // Use array to store the image value
-    float imageArr[padded_image.rows][padded_image.cols];
+    float imageArr[n_rows][n_cols];
 
-    for(int i = 0 ;i < padded_image.rows; ++i)
-      for(int j = 0; j < padded_image.cols; ++j)
-        imageArr[i][j] = padded_image.at<float>(i, j);
+    for (int i = 0; i < padded_image.rows; ++i)
+        for (int j = 0; j < padded_image.cols; ++j)
+            imageArr[i][j] = padded_image.at<float>(i, j);
 
     // loop through calculating G_x and G_y
     // mag is sqrt(G_x^2 + G_y^2)
@@ -127,38 +111,37 @@ Mat sobel_array(Mat padded_image)
         for (int c = 0; c < n_cols; c++)
         {
             float mag_x = imageArr[r][c] * g_x[0][0] +
-                          imageArr[r][c+1] * g_x[0][1] +
-                          imageArr[r][c+2] * g_x[0][2] +
-                          imageArr[r+1][c] * g_x[1][0] +
-                          imageArr[r+1][c+1] * g_x[1][1] +
-                          imageArr[r+1][c+2] * g_x[1][2] +
-                          imageArr[r+2][c] * g_x[2][0] +
-                          imageArr[r+2][c+1] * g_x[2][1] +
-                          imageArr[r+2][c+2] * g_x[2][2];
+                          imageArr[r][c + 1] * g_x[0][1] +
+                          imageArr[r][c + 2] * g_x[0][2] +
+                          imageArr[r + 1][c] * g_x[1][0] +
+                          imageArr[r + 1][c + 1] * g_x[1][1] +
+                          imageArr[r + 1][c + 2] * g_x[1][2] +
+                          imageArr[r + 2][c] * g_x[2][0] +
+                          imageArr[r + 2][c + 1] * g_x[2][1] +
+                          imageArr[r + 2][c + 2] * g_x[2][2];
 
             float mag_y = imageArr[r][c] * g_y[0][0] +
-                          imageArr[r][c+1] * g_y[0][1] +
-                          imageArr[r][c+2] * g_y[0][2] +
-                          imageArr[r+1][c] * g_y[1][0] +
-                          imageArr[r+1][c+1] * g_y[1][1] +
-                          imageArr[r+1][c+2] * g_y[1][2] +
-                          imageArr[r+2][c] * g_y[2][0] +
-                          imageArr[r+2][c+1] * g_y[2][1] +
-                          imageArr[r+2][c+2] * g_y[2][2];
+                          imageArr[r][c + 1] * g_y[0][1] +
+                          imageArr[r][c + 2] * g_y[0][2] +
+                          imageArr[r + 1][c] * g_y[1][0] +
+                          imageArr[r + 1][c + 1] * g_y[1][1] +
+                          imageArr[r + 1][c + 2] * g_y[1][2] +
+                          imageArr[r + 2][c] * g_y[2][0] +
+                          imageArr[r + 2][c + 1] * g_y[2][1] +
+                          imageArr[r + 2][c + 2] * g_y[2][2];
 
-			// Instead of Mat, store the value into an array
-			sobel_array[r][c] = sqrt(pow(mag_x, 2) + pow(mag_y, 2));
+            // Instead of Mat, store the value into an array
+            sobel_array[r][c] = sqrt(pow(mag_x, 2) + pow(mag_y, 2));
         }
     }
 
-	// Convert array to Mat, base on documentation, this function only create a header that points to the data 
-	Mat sobel_image = Mat(n_rows, n_cols, CV_64F, sobel_array);
+    // Convert array to Mat, base on documentation, this function only create a header that points to the data
+    Mat sobel_image = Mat(n_rows, n_cols, CV_32F, sobel_array);
 
     normalize(sobel_image, sobel_image, 0, 255, NORM_MINMAX, CV_8UC1);
-    
+
     return sobel_image;
 }
-
 
 int main(int argc, char **argv)
 {
@@ -169,16 +152,36 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    Mat padded_img = preprocessing(argv[1]);
+    // read in image as grayscale
+    // Mat is an OpenCV data structure
+    Mat raw_image = imread(argv[1], IMREAD_GRAYSCALE);
 
-    if (padded_img.empty())
+    if (raw_image.empty())
     {
+        std::cout << "Could not read image: " << argv[1] << std::endl;
         return 1;
     }
 
-    Mat sobel_img = sobel(padded_img);
+    // convert image to CV_32F (equivalent to a float)
+    Mat image;
+    raw_image.convertTo(image, CV_32F);
 
-    imshow("Detected Edges", sobel_img);
+    // ----- Initial Sobel Implementation -----
+
+    Mat orig_padded_img = preprocessing(image);
+
+    Mat orig_sobel_img = sobel(orig_padded_img);
+
+    imshow("Detected Edges", orig_sobel_img);
+    waitKey(0);
+
+    // ----- Array Sobel Implementation -----
+
+    Mat arr_padded_img = preprocessing(image);
+
+    Mat arr_sobel_img = sobel_array(arr_padded_img);
+
+    imshow("Detected Edges", arr_sobel_img);
     waitKey(0);
 
     return 0;
