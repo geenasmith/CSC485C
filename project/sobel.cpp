1d14dbf5eccba2874597ecf508362f6acf04f366
Jason Parker <jasparke@uvic.ca>
2021-03-13 15:22:58 -0800

    idk

--- project/sobel.cpp
+++ project/sobel.cpp
@@ -1433,27 +1433,101 @@ static void BENCH_SobelHardcodePowAndSqrtImplementation(benchmark::State &state)
         // benchmark::DoNotOptimize(output_array);
     }
 }
+static void BENCH_SobelHardcodePowAndSqrtImplementationTrim(benchmark::State &state)
+{
+    // read in image as grayscale OpenCV Mat Object
+    Mat input_image = imread(FILENAME, IMREAD_GRAYSCALE);
 
+    Mat resized_image = resizeImage(input_image, state.range(0));
+
+    // convert image to CV_8UC1 (uint8)
+    Mat image;
+    resized_image.convertTo(image, CV_8UC1);
+
+    // Filtered image definitions
+    int n_rows = image.rows;
+    int n_cols = image.cols;
+
+    Mat padded_image = preprocessing(image);
+
+    // Use array to store the image value
+    uint8_t padded_array[padded_image.rows][padded_image.cols];
+    for (int i = 0; i < padded_image.rows; ++i)
+        for (int j = 0; j < padded_image.cols; ++j)
+            padded_array[i][j] = padded_image.at<float>(i, j);
+
+    // Benchmark includes convolution and normalization back to [0,255]
+    for (auto _ : state)
+    {
+        float output_array[n_rows][n_cols];
+        float max = -INFINITY;
+        float min = INFINITY;
+        for (int r = 0; r < n_rows; r++)
+        {
+            for (int c = 0; c < n_cols; c++)
+            {
+                float mag_x = padded_array[r][c] -
+                              padded_array[r][c + 2] +
+                              padded_array[r + 1][c] * 2 +
+                              padded_array[r + 1][c + 2] * -2 +
+                              padded_array[r + 2][c] -
+                              padded_array[r + 2][c + 2];
+
+                float mag_y = padded_array[r][c] +
+                              padded_array[r][c + 1] * 2 +
+                              padded_array[r][c + 2] -
+                              padded_array[r + 2][c] +
+                              padded_array[r + 2][c + 1] * -2 -
+                              padded_array[r + 2][c + 2];
+
+                // Instead of Mat, store the value into an array
+                output_array[r][c] = sqrt_impl((mag_x * mag_x) + (mag_y * mag_y));
+
+                if (output_array[r][c] > max)
+                {
+                    max = output_array[r][c];
+                }
+                if (output_array[r][c] < min)
+                {
+                    min = output_array[r][c];
+                }
+            }
+        }
+
+        // Implement our own normalization
+        // For each pixel I, I_norm = (I-Min) * (newMax-newMin) / (Max-Min) + newMin
+        for (int r = 0; r < n_rows; r++)
+        {
+            for (int c = 0; c < n_cols; c++)
+            {
+                output_array[r][c] = (output_array[r][c] - min) * (255) / (max - min);
+            }
+        }
+
+        // benchmark::DoNotOptimize(output_array);
+    }
+}
 /*
 Can only pass in arguments to benchmark function that are integers.
 To run on different sized images, will pass in how small to shrink the image.
 Eg. passing in 0 will resize image by 1/2^0 on each axis, 1 will resize to 1/2^1 etc.
 */
-BENCHMARK(BENCH_SobelOriginalMatImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelOriginalNormalizationImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelArrayImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelVectorImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelHardcodeKernelsImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelHardcodeKernelsNormalizationImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_Sobeluint8InputImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_Sobeluint8Input1DArray)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_Sobeluint8MathOperationReorder)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_Sobeluint8Input1DArrayMathOperationReorder)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_Sobeluint8InputImplementationLoopUnroll)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelMagIntsImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelHardcodePowImplementation)->DenseRange(0, DENSERANGEEND, 1);
-BENCHMARK(BENCH_SobelCombineMaxMinImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelOriginalMatImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelOriginalNormalizationImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelArrayImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelVectorImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelHardcodeKernelsImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelHardcodeKernelsNormalizationImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_Sobeluint8InputImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_Sobeluint8Input1DArray)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_Sobeluint8MathOperationReorder)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_Sobeluint8Input1DArrayMathOperationReorder)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_Sobeluint8InputImplementationLoopUnroll)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelMagIntsImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelHardcodePowImplementation)->DenseRange(0, DENSERANGEEND, 1);
+// BENCHMARK(BENCH_SobelCombineMaxMinImplementation)->DenseRange(0, DENSERANGEEND, 1);
 BENCHMARK(BENCH_SobelHardcodePowAndSqrtImplementation)->DenseRange(0, DENSERANGEEND, 1);
+BENCHMARK(BENCH_SobelHardcodePowAndSqrtImplementationTrim)->DenseRange(0, DENSERANGEEND, 1);
 
 // Calls and runs the benchmark program
 BENCHMARK_MAIN();
