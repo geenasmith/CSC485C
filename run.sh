a851a306a473038472e194c6e3f2654c1309a8d6
Evan Guo <evanguo97@uvic.ca>
2021-03-21 15:59:40 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 
 git pull
 cd project
-g++ -std=c++11 sobel.cpp -o sobel `pkg-config --cflags --libs opencv4` -isystem ../benchmark/include -L../benchmark/build/src -lbenchmark -lpthread
-./sobel --benchmark_out=benchmarking.csv --benchmark_out_format=csv
+mkdir outputs
+g++ -std=c++17 sobel_simd.cpp -o sobel `pkg-config --cflags --libs opencv` -mavx -march=native
+
+perf stat -o ./outputs/simd_20000_basic.out -e task-clock,cycles,instructions,L1-dcache-loads,L1-dcache-load-misses,LLC-loads,LLC-load-misses ./sobel
+perf stat -o ./outputs/simd_20000_simd.out -e fp_arith_inst_retired.scalar_single,fp_arith_inst_retired.scalar_double,fp_arith_inst_retired.128b_packed_single,fp_arith_inst_retired.256b_packed_single ./sobel
