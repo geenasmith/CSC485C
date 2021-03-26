0e2309c60fa58d1555aed7bdce3776f4d9fa8942
Jason Parker <jasparke@uvic.ca>
2021-03-26 14:43:47 -0700

    update new imageset

--- project/resizer.cpp
+++ project/resizer.cpp
@@ -11,17 +11,24 @@ using namespace std;
 
 int main()
 {
-    Mat input_image = imread("sampleset/frac8.png", IMREAD_COLOR);
+    Mat input_image = imread("sampleset/4000x2250.jpg", IMREAD_COLOR);
     cout << "input_img" << input_image.cols << "x" << input_image.rows << endl;
 
-    for (int i = 0; i < 6; i++) {
+    // for (int i = 1; i < 9; i++) {
 
-        Mat resized_image;
-        resize(input_image, resized_image, Size(input_image.cols - i, input_image.rows - i), 0, 0);
-        string name = format("sampleset/test/frac8-%dx%d.png", resized_image.cols, resized_image.rows);
-        cout << "output: "<<name<< " "<< input_image.cols << "x" << input_image.rows << endl;
-        imwrite(name, resized_image);
-    }
+    //     Mat resized_image;
+    //     resize(input_image, resized_image, Size((int)(4000 * i * 0.1),(int)(2250 * i * 0.1)), 0, 0);
+    //     string name = format("sampleset/new-%d-%d.jpg", resized_image.cols, resized_image.rows);
+    //     cout << "output: "<<name<< " "<< input_image.cols << "x" << input_image.rows << endl;
+    //     imwrite(name, resized_image);
+    // }
+
+
+    Mat resized_image;
+
+    resize(input_image, resized_image, Size(200,114), 0, 0);
+    string name = format("sampleset/new-%d-%d.jpg", resized_image.cols, resized_image.rows);
+    imwrite(name, resized_image);
 
     return 0;
 }
